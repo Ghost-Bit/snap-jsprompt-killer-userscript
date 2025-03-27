472a3aa19fa5de27a1a688931f39c0f02b486338
Alice Margatroid <54186126+Ghost-Bit@users.noreply.github.com>
2025-03-27 08:13:37 -0400

    the thing
    added the thing.

---
+++ jsprompt-killer.js
@@ -0,0 +1,80 @@
+// ==UserScript==
+// @name        jsprompt-killer
+// @namespace   ensnapbkjs
+// @include     https://snap.berkeley.edu/*
+// @version     (please read the contents of this userscript before using it)
+// @grant       none
+// @run-at document-end
+// ==/UserScript==
+  //!!!!! I suggest you read everything here before doing anything. !!!!!
+
+(function() {
+    
+  //WARNING!WARNING!WARNING!WARNING!WARNING!WARNING!WARNING!WARNING!WARNING!WARNING!WARNING!WARNING!
+  //Use at your own risk, folks. using this DOES make you vulnerable to exploits previously
+  //established. (or not established, because bh is hesitant to mention anything that'd help protect 
+  //yourselves and just pushed a questionable, all-encompassing "fix" and left it that way T_T 
+  //(it was a phishing attack))
+  //WARNING!WARNING!WARNING!WARNING!WARNING!WARNING!WARNING!WARNING!WARNING!WARNING!WARNING!WARNING! 
+  
+  console.log("Javascript has been auto-enabled,");
+  {Process.prototype.enableJS = true};
+  
+  //to bh, there are better ways to do this. your paranoid, hasty solution is annoying and upsets 
+  //people like me who want little more than to simply click the flag and have the code run as it is.
+  
+  //The least you can do is make this possible outside of the editor so some poor soul doesn't have to
+  //load someone's project TWICE. a warning will suffice, add a button to enable JS and add a warning.
+  //everyone isn't as dumb as you think and you can't even admit you've tried it. trust your userbase.
+  //Not everyone here is a child. Scratch and Snap! are different entities and should be treated as
+  //such with respect to their inteligence.
+  
+  //This has also killed the greater feasibility of online games, something i found really neat about
+  //Snap! a few years ago. way to go, bh, not like a lot of them were popular anyways...
+  
+  //Thank you to DarDoro for the basis for this extension.
+
+  //I personally feel that what you've done has stripped something very very unique from Snap! that
+  //made it what it is and not just just another extended Scratch mod. your blessed extensions aren't
+  //the point. we should be able to do as we please with the tools we are given. bad actors should be 
+  //the ones getting punished for this, not everyone.
+  
+  //And don't act like this is the only way. Do you really think this is any more secure? It's 
+  //dissuasive, yes, but globally by attacking the chances that ANYONE uses javascript AT ALL!
+  //The least you can do is make people's lives easier by making their own projects have JS 
+  //automatically enabled! What, is THEIR project somehow going to just magically load up with a new 
+  //peice of code they didn't add that steals their data? really? get real, the toggle switch is a very
+  //poor solution.
+  
+  //And as such i saw this, and uploaded it in the hopes that it reaches those who search for it.
+  //They deserve the pure freedom that was present before the Enable Javascript feature was added.
+  //Especially on the editorless page of a public project, and even more so the editor of their own
+  //self-saved projects.
+  
+  //I take no direct responsibility for anythng that happens after this. You looked for this, You've 
+  //seen the forums, and if you haven't, go read that stuff first so you know what you're getting into.
+  
+  //Regardless of my responsibility...
+  //Stay safe, if you're scared, it's okay to ask someone. Look at the javascript first and perhaps ask
+  //ChatGPT if the code is safe to run if you don't understand it. it's not 100% but it's certainly not
+  //nothing. you're safest checking things before running with this enabled.
+  
+  //Remember to right click and choose edit... on a block to view it's contents. and if you see 
+  //something, say something. report the project for it's contents and have it taken down by the 
+  //moderators.
+  
+  //I share all this independent of the forum so any conversation operates on my terms as a member of
+  //the Snap! community. nothing closes that i see hasn't been resolved properly because i'm squeamish
+  //and uncomfortable talking about it. i declare the Issues page of this userscript as a free and safe 
+  //zone without unreasonable moderation interruption to discuss what i've done, the JS vulnerabilities, 
+  //actual solutions, why i honestly might be a bit of an ass about this, and  the philosophies of any
+  //related part of the Enable JS situation.
+
+  //Sorry if i do end up coming off as an asshole, but i hate the way this was done and feel like
+  //there should be more thought put into the fixing of this situation than there's currently been.
+  
+  //And to any bad actors; please reconsider what you're doing and find an actual job or hobby. you're
+  //just being an asshole. what do you have to gain from stealing data from any individual on Snap! of 
+  //all places? You're a bully and an ass, plain and simple, get a life and leave people alone. 
+  
+})();
